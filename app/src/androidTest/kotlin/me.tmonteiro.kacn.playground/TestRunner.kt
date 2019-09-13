a5eea5097c621ebd8cb21dd520eeab5cad08a0de
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2019-09-13 16:13:50 -0300

    Updates TestRunner

--- app/src/androidTest/kotlin/me.tmonteiro.kacn.playground/TestRunner.kt
+++ app/src/androidTest/kotlin/me.tmonteiro.kacn.playground/TestRunner.kt
@@ -4,16 +4,10 @@ import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 
-class TestRunnerAbc : AndroidJUnitRunner() {
+class TestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
-//        DexOpener.install(this)
         return super.newApplication(cl, App::class.java.name, context)
     }
 }
 
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-//        AndroidThreeTen.init(this)
-    }
-}
+class App : Application()
